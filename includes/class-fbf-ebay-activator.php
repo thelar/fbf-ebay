e3766059854700b54374920863084b8e8d34b788
Kevin Price-Ward <kevin.price-ward@4x4tyres.co.uk>
2021-01-19 10:35:54 +0000

    initial commit

---
+++ includes/class-fbf-ebay-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://4x4tyres.co.uk
+ * @since      1.0.0
+ *
+ * @package    Fbf_Ebay
+ * @subpackage Fbf_Ebay/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Fbf_Ebay
+ * @subpackage Fbf_Ebay/includes
+ * @author     Kevin Price-Ward <kevin.price-ward@4x4tyres.co.uk>
+ */
+class Fbf_Ebay_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
